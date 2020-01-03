360856e2bf8bfbf97a06eac81b51f53b3aa5111e
Diguix <rtteixeira1@gmail.com>
2020-01-03 12:01:55 -0300

    update screen insert icon for action

--- src/components/listar/index.js
+++ src/components/listar/index.js
@@ -1,119 +1,120 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react';
 import {
-    View, Text, FlatList, List, TextInput, Button, TouchableOpacity, Alert
-} from 'react-native'
+  View,
+  Text,
+  FlatList,
+  List,
+  TextInput,
+  Button,
+  TouchableOpacity,
+  Alert,
+  Image,
+} from 'react-native';
 
-import api from '../../services/api'
-
-
+import api from '../../services/api';
 
 export default class ListaResponsavel extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            data: []
-        }
-
-        this._handleLista = this._handleLista.bind(this)
-    }
-    static navigationOptions = {
-        // title: 'Lista',
-        headerStyle: {
-            backgroundColor: '#5DBCD2',
-        },
-        headerBackTitle: null,
-        headerTintColor: '#fff',
-        headerTitleStyle: {
-            fontWeight: 'bold',
-        },
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
     };
 
-    componentDidMount() {
-        this._handleLista()
+    this._handleLista = this._handleLista.bind(this);
+  }
+  static navigationOptions = {
+    // title: 'Lista',
+    headerStyle: {
+      backgroundColor: '#5DBCD2',
+    },
+    headerBackTitle: null,
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+      fontWeight: 'bold',
+    },
+  };
+
+  componentDidMount() {
+    this._handleLista();
+  }
+
+  _handleLista = async () => {
+    try {
+      let response = await api.listaResponsavel();
+
+      let res = this.setState(
+        {
+          data: response,
+        },
+        function() {},
+      );
+      return res;
+    } catch (error) {
+      return error;
     }
+  };
 
-    _handleLista = async () => {
-        try {
-            let rr = await api.listaResponsavel()
+  _handleChangeText = text => {
+    this.setState({dtNome: text});
+  };
 
-            let res = this.setState({
-                data: rr
-            }, function () {
+  handleExcluir() {
+    Alert.alert('ATENÇÃO! ', 'A Exclusão será permanente', [
+      {
+        text: 'Cancel',
+        onPress: () => console.log('Cancel Pressed'),
+      },
+      {text: 'OK', onPress: () => console.log('OK Pressed')},
+    ]);
+  }
 
-            });
-            return res
-        } catch (error) {
-            return error
-        }
-    }
+  render() {
+    return (
+      <View>
+        <FlatList
+          keyExtractor={item => item._id}
+          data={this.state.data}
+          renderItem={({item}) => (
+            <View style={{margin: 15, padding: 15}}>
+              <TextInput
+                style={{color: '#000000'}}
+                onChangeText={text => this._handleChangeText(text)}
+                value={item.nome}
+              />
+              <TextInput
+                style={{color: '#000000'}}
+                onChangeText={text => this._handleChangeText(text)}
+                value={item.email}
+              />
+              <View
+                style={{
+                  flexDirection: 'row-reverse',
+                  marginTop: -34,
+                }}>
+                <TouchableOpacity
+                  onPress={() =>
+                    this.props.navigation.navigate(
+                      'EditarResponsavel',
+                      this.state.data,
+                    )
+                  }>
+                  <Image
+                    style={{width: 30, height: 30}}
+                    source={require('../../imagem/iconListar/editar.png')}
+                  />
+                </TouchableOpacity>
 
-    _handleChangeText = (text) => {
-
-        this.setState({ dtNome: text });
-    };
-
-    handleExcluir() {
-        Alert.alert(
-            'ATENÇÃO! ',
-            'A Exclusão será permanente',
-            [
-            {
-                text: 'Cancel',
-                onPress: () => console.log('Cancel Pressed'),
-            },
-            { text: 'OK', onPress: () => console.log('OK Pressed') },
-            ]
-        )
-    }
-
-    render() {
-
-
-        return (
-            <View >
-                <FlatList
-                    // data={this.state.data}
-                    data={this.state.data}
-                    renderItem={({ item }) => (
-                        <View style={{ marginTop: 10, padding: 20, flex: 1,  }}>
-                            <TextInput
-                                style={{ flex: 1 }}
-                                onChangeText={text => this._handleChangeText(text)}
-                                value={item.nome}
-                            />
-                            <View style={{
-                                flex: 1, flexDirection: 'row-reverse', justifyContent: 'center', alignItems: 'center'
-
-                            }}>
-                                <TouchableOpacity
-                                    style={{ width: 50 }}
-                                    onPress={() => this.props.navigation.navigate('EditarResponsavel', this.state.data)}
-                                >
-                                    <Text>Editar</Text>
-                                </TouchableOpacity>
-
-                                <TouchableOpacity
-                                    style={{ width: 50 }}
-                                    onPress={this.handleExcluir}
-                                >
-                                    <Text>Excluir</Text>
-                                </TouchableOpacity>
-
-                                <TextInput
-                                    style={{ flex: 1 }}
-                                    onChangeText={text => this._handleChangeText(text)}
-                                    value={item.email}
-                                />
-
-
-
-
-                            </View>
-                        </View>
-                    )}
-                    keyExtractor={item => item._id}
-                />
+                <TouchableOpacity onPress={this.handleExcluir}>
+                  <Image
+                    style={{width: 30, height: 30}}
+                    source={require('../../imagem/iconListar/excluir.png')}
+                  />
+                </TouchableOpacity>
+              </View>
             </View>
-        )
-    }
+          )}
+        />
+      </View>
+    );
+  }
 }
