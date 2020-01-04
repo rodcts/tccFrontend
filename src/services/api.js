4fbdb861e4b0d828640560b675946ee92f138a3f
Diguix <rtteixeira1@gmail.com>
2020-01-04 17:29:09 -0300

    update src/image and src/services

--- src/services/api.js
+++ src/services/api.js
@@ -1,30 +1,48 @@
-import axios from '../../node_modules/axios'
+import axios from '../../node_modules/axios';
 
 const api = axios.create({
-    // baseURL: 'http://localhost:3000/',
-    baseURL: 'mongodb+srv://m001-student:ctrlalt1@cluster0-livgn.mongodb.net/'
-})
+  // baseURL: 'http://localhost:3000/',
+  baseURL: 'mongodb+srv://m001-student:ctrlalt1@cluster0-livgn.mongodb.net/',
+});
 
 async function listaResponsavel() {
-    try {
-        let res = await axios.get('http://localhost:3000/responsavel/list')
+  try {
+    let res = await axios.get('http://localhost:3000/responsavel/list');
 
-
-        for(let key in res) {
-            if (res.hasOwnProperty(key)) {
-                let element = res[key];
-                return element
-            }
-        }
-            
-
-    } catch (error) {
-        return error
+    for (let key in res) {
+      if (res.hasOwnProperty(key)) {
+        let element = res[key];
+        return element;
+      }
     }
+  } catch (error) {
+    return error;
+  }
 }
 
+async function buscarResponsavel(cpf) {
+  try {
+    let res = await axios.get(`http://localhost:3000/responsavel/find/${cpf}`)
+    return res
+  } catch (e) {
+    return e;
+  }
+}
+
+async function atualizarResponsavel(id) {
+  try {
+    let res = await axios.put(
+      `http://localhost:3000/responsavel/update/${id}`,
+    );
+    return res
+  } catch (e) {
+    return e;
+  }
+}
 
 export default {
-    api,
-    listaResponsavel,
-}
+  api,
+  listaResponsavel,
+  atualizarResponsavel,
+  buscarResponsavel,
+};
